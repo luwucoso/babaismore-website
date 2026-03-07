b20388bc2cd985b4dd40f8439870bcaa86f47254
luco <lucozy@proton.me>
2026-03-07 14:34:26 -0300

    babu is component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Navbar from '@/components/Navbar/Bar.vue'
+import Babu from '@/components/Babu.vue'
 </script>
 
 <template>
@@ -9,8 +10,7 @@ import Navbar from '@/components/Navbar/Bar.vue'
         <p> Placeholder paragraph
         </p>
 
-        <div class="babu_blue">Button</div>
-        <div class="babu_pink">Button</div>
+        <Babu />
 
         <div class="main">
                 <router-view />
@@ -21,68 +21,5 @@ import Navbar from '@/components/Navbar/Bar.vue'
 .main {
         /* if you change this, change components/navbar/Bar.vue as well */
         margin-top: 10vh;
-}
-
-/* Implementation done by @xnorvey on Discord*/
-.babu_blue {
-        border-image-slice: 8 8 8 8 fill;
-        border-image-width: 10px 10px 10px 10px;
-        border-image-outset: 0px 0px 0px 0px;
-        border-image-repeat: round round;
-        border-image-source: url("/public/images/buttons/button_blue.png");
-        border-style: solid;
-        image-rendering: crisp-edges;
-        font-family: "Baba UI", sans-serif;
-        color: white;
-        padding: 4.5px;
-        padding-left: 40px;
-        padding-right: 40px;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        cursor: pointer;
-}
-
-.babu_blue:hover {
-        border-image-source: url("/public/images/buttons/button_blue_hover.png");
-}
-
-.babu_blue:active {
-        border-image-source: url("/public/images/buttons/button_blue_press.png");
-}
-
-.babu_blue:disabled {
-        border-image-source: url("/public/images/buttons/button_disabled.png");
-}
-
-.babu_pink {
-        border-image-slice: 8 8 8 8 fill;
-        border-image-width: 10px 10px 10px 10px;
-        border-image-outset: 0px 0px 0px 0px;
-        border-image-repeat: round round;
-        border-image-source: url("/public/images/buttons/button_pink.png");
-        border-style: solid;
-        image-rendering: crisp-edges;
-        font-family: "Baba UI", sans-serif;
-        color: white;
-        padding: 4.5px;
-        padding-left: 40px;
-        padding-right: 40px;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        cursor: pointer;
-}
-
-.babu_pink:hover {
-        border-image-source: url("/public/images/buttons/button_pink_hover.png");
-}
-
-.babu_pink:active {
-        border-image-source: url("/public/images/buttons/button_pink_press.png");
-}
-
-.babu_pink:disabled {
-        border-image-source: url("/public/images/buttons/button_disabled.png");
 }
 </style>
---
+++ src/components/Babu.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<div class="babu">Button</div>
+</template>
+
+<style scoped>
+/* Implementation done by @xnorvey on Discord*/
+.babu {
+	border-image-slice: 8 8 8 8 fill;
+	border-image-width: 10px 10px 10px 10px;
+	border-image-outset: 0px 0px 0px 0px;
+	border-image-repeat: round round;
+	border-image-source: url("/public/images/buttons/button_blue.png");
+	border-style: solid;
+	image-rendering: crisp-edges;
+	font-family: "Baba UI", sans-serif;
+	color: white;
+	padding: 4.5px;
+	padding-left: 40px;
+	padding-right: 40px;
+	cursor: pointer;
+}
+
+.babu:hover {
+	border-image-source: url("/public/images/buttons/button_blue_hover.png");
+}
+
+.babu:active {
+	border-image-source: url("/public/images/buttons/button_blue_press.png");
+}
+
+.babu:disabled {
+	border-image-source: url("/public/images/buttons/button_disabled.png");
+}
+</style>
